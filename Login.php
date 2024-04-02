b301e336af7580035fad971c65def5c465b139f3
Anthony Acosta <76180453+mickrose32@users.noreply.github.com>
2024-04-02 15:26:48 -0600

    Added a authentication page to the login page.

--- Login.php
+++ Login.php
@@ -14,23 +14,19 @@
 </head>
 
 <body>
-    <div class="d-felx justify-content-center align-items-center" style="min-height: 100vh">
-        <form class="p-5 rounded shadow" style="max-width: 30rem; width: 100%">
+    <div class="d-flex justify-content-center align-items-center" style="min-height: 100vh">
+        <form class="p-5 rounded shadow" style="max-width: 30rem; width: 100%" method="POST" action="php/auth.php">
             <h1 class="text-center display-4 pb-5">Login</h1>
             <div class="mb-3">
                 <label for="exampleInputEmail1" class="form-label">Email address</label>
                 <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
             </div>
             <div class="mb-3">
                 <label for="exampleInputPassword1" class="form-label">Password</label>
                 <input type="password" class="form-control" id="exampleInputPassword1">
             </div>
-            <div class="mb-3 form-check">
-                <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                <label class="form-check-label" for="exampleCheck1">Check me out</label>
-            </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
+            <button type="submit" class="btn btn-primary">Login</button>
+            <a href="index.php">Store</a>
         </form>
     </div>
 </body>
