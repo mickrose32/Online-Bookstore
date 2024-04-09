595fe381b2966fca5a996c58678f2cb2d6c338e5
Anthony Acosta <76180453+mickrose32@users.noreply.github.com>
2024-04-08 19:17:16 -0600

    Completed the data.sql file which will be used to add the information regarding the books

--- MySQL/data.sql
+++ MySQL/data.sql
@@ -6,3 +6,14 @@ VALUES (#)
 INSERT INTO books (title, author_id, publisher, publication_data, isbn, genre, price)
 VALUES (#)
 
+-- Sample data for customers
+INSERT INTO customers (name, email, address, phone_number, payment_information)
+VALUES (#)
+
+--Sample data for orders
+INSERT INTO orders (data, order_number, customer_id, status)
+VALUES (#)
+
+-- Sample data for transactions
+INSERT INTO transactions (order_id, payment_amount, payment_date, payment_method)
+VALUES (#)
